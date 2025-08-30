130d6ccb4d7b22cccd14d4526038c6d3782f1dcb
Akshay-a <apsingiakshay46@gmail.com>
2025-08-30 21:36:38 +1000

    Added error boundary to make it more UI responsive and avoid screen stuck

--- App.tsx
+++ App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import 'react-native-gesture-handler';
 
 import AppNavigator from './src/navigation/AppNavigator';
+import ErrorBoundary from './src/components/ErrorBoundary';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <ErrorBoundary>
+      <AppNavigator />
+    </ErrorBoundary>
+  );
 }
